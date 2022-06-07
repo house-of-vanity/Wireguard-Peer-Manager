e6c28095107960abe903a1914245f3e515402480
root <root@anon.hexor.ru>
2022-06-07 15:55:09 +0000

    Added stat command

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ else:
 
 def _help(update, context):
     update.message.reply_text(
-        '<b>Help:</b>\n <b>*</b> /add <i>peer name</i>\n <b>*</b> /del <i>peer name</i>\n <b>*</b> /list [<i>peer name</i>]',
+        '<b>Help:</b>\n <b>*</b> /add <i>peer name</i>\n <b>*</b> /del <i>peer name</i>\n <b>*</b> /list [<i>peer name</i>]\n<b>*</b> /status - show status',
         parse_mode='HTML',
         disable_web_page_preview=True)
 
